162356ee8dd2a0c4923cd6e410a34515d57552c0
bruce <bruce>
2023-04-19 16:10:24 +0800

    add comment

--- src/lesson10/advanced/serializable/Animal.java
+++ src/lesson10/advanced/serializable/Animal.java
@@ -5,6 +5,10 @@ import java.io.Serializable;
 
 /**
  * transient 和 static 不會被序列化
+ *
+ * serialVersionUID 為序列化時固定的序號，如果不寫會隨機生成，但有可能會序列化失敗
+ * 假設 serialVersionUID 為 1，序列化之後生成檔案了，修改成 10
+ * 這時反序列會失敗，因為序號不相同無法反序列化
  */
 public class Animal implements Serializable {
     @Serial // JDK 14
